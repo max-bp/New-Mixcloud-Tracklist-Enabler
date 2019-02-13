caa9f06f401166f46847ca4c165098a74078d640
Max <f0r-spam@ya.ru>
2019-02-13 16:06:09 +0300

    Update mix.js

--- mix.js
+++ mix.js
@@ -31,8 +31,6 @@ var jqsrc = '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
             var thispath = window.location.pathname.split('/'), slug = cleanArray(thispath),artist=slug[0];
                 slug = slug[1];
                 getTrackList(artist,slug);
-            break;
-        }
     },
     gmloadScript = function (sScriptSrc) {
         var oHead = document.getElementsByTagName('head')[0],
